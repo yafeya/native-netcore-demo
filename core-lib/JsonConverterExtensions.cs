d135455f8a64ea748555a0b745018c0605226a8a
YANG,FEIYANG (K-China,ex1) <fei-yang.yang@keysight.com>
2018-09-21 13:13:00 +0800

    init

---
+++ core-lib/JsonConverterExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace core_lib
+{
+    public static class JsonConverterExtensions
+    {
+        public static string ToJson<T>(this T instance) where T : class
+        {
+            var result = string.Empty;
+
+            try
+            {
+                result = JsonConvert.SerializeObject(instance);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return result;
+        }
+
+        public static T FromJson<T>(this string json) where T : class
+        {
+            T result = null;
+            try
+            {
+                result = JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return result;
+        }
+    }
+}
---
+++ native-netcore-demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using core_lib;
+using System;
+
+namespace native_netcore_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert Json!");
+
+            var user1 = new User { Name = "Frank", Age = 18 };
+            var json1 = user1.ToJson();
+            Console.WriteLine(json1);
+            var back1 = json1.FromJson<User>();
+            Console.WriteLine(back1.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
